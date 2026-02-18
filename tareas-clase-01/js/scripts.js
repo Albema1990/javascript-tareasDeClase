4be6be5549286faa394c8282f5b8fd171c576a6d
Álvaro Benitez Mayorga <alvarobenitezmayorga@gmail.com>
2026-02-18 21:56:19 +0100

    clase 01

---
+++ tareas-clase-01/js/scripts.js
@@ -0,0 +1,33 @@
+// // Ejercicio 1 clase 01
+
+// console.log("Mi nombre es Álvaro")
+// console.log("Tengo 36 años");
+
+
+// // Ejercicio 2 clase 01
+
+// let nombre = "Álvaro";
+// let apellido = "Benítez";
+
+// console.log("Me llamo" + nombre + apellido); // Esto sale por consola todo junto
+// console.log("Me llamo", nombre, apellido);
+// console.log(`Me llamo ${nombre} ${apellido}`);
+
+
+// // Ejercicio 3 clase 01
+
+// let texto = "Hola";
+// let num = 7;
+// let direccion;
+
+// console.log(texto);
+// console.log(num);
+// console.log(direccion);
+
+// Ejercicio 4 clase 01
+
+// const precio = 10.99;
+// const cantidad = 3;
+
+// const total = precio * cantidad;
+// console.log("El total es:" , total);

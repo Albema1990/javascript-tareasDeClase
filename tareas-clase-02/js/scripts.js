1e6eabcd3d4bcbd0234340e1fb1223d7509f0b1f
Álvaro Benitez Mayorga <alvarobenitezmayorga@gmail.com>
2026-02-19 20:53:19 +0100

    condicionales

---
+++ tareas-clase-02/js/scripts.js
@@ -0,0 +1,63 @@
+// // Valor booleano
+
+// // let verdadero = true;
+// // let falso = false;
+
+// // Operador de asignación
+
+// // console.log(5 == 5);
+// // console.log(5 === "5");
+
+// // Ejercicio 1 clase 02
+
+// if (`8` > `5`) {
+//     console.log("8 es mayor que 5");
+// }
+
+// if (`4` > `2`) {
+//     console.log("4 es mayor que 2");
+// }
+
+// // Ejercicio 2 clase 02
+
+// let edad = 16;
+
+// const mayorEdad = 18;
+
+// if (edad >= mayorEdad){
+//     console.log("Eres mayor de edad!")
+// }else console.log("Eres muy pequeño");
+
+// let temperatura = 36;
+
+// if(temperatura < 5){
+//     console.log("Hace frio");
+// }else {
+//     console.log(temperatura, "grados son demasiados!");
+// }
+
+// Ejercicio 3 clase 02
+
+// let nota = 3;
+
+// if (nota >=5){
+//     console.log("Aprobado!");
+// }else {
+//     console.log("Lo siento, estas suspenso");
+// }
+
+// Ejercicio 4 clase 02
+
+let edad = 38;
+
+if (typeof edad == "number"){
+    if(edad < 13){
+        console.log("Niño");
+    }else if (edad < 18){
+        console.log("Adolescente");
+    }else {
+        console.log("Adulto");
+    }
+}else {
+    console.log("Valor equivocado, inserta un numero");
+}

0b71cf95dde16327425392df2b8125f7f0a8cd05
Álvaro Benitez Mayorga <alvarobenitezmayorga@gmail.com>
2026-02-19 19:32:43 +0100

    clase 02

--- tareas-clase-01/js/scripts.js
+++ tareas-clase-01/js/scripts.js
@@ -1,24 +1,24 @@
 // // Ejercicio 1 clase 01
 
-// console.log("Mi nombre es Álvaro")
-// console.log("Tengo 36 años");
+console.log("Mi nombre es Álvaro")
+console.log("Tengo 36 años");
 
 
 // // Ejercicio 2 clase 01
 
-// let nombre = "Álvaro";
-// let apellido = "Benítez";
+let nombre = "Álvaro";
+let apellido = "Benítez";
 
-// console.log("Me llamo" + nombre + apellido); // Esto sale por consola todo junto
-// console.log("Me llamo", nombre, apellido);
-// console.log(`Me llamo ${nombre} ${apellido}`);
+console.log("Me llamo" + nombre + apellido); // Esto sale por consola todo junto
+console.log("Me llamo", nombre, apellido);
+console.log(`Me llamo ${nombre} ${apellido}`);
 
 
 // // Ejercicio 3 clase 01
 
-// let texto = "Hola";
-// let num = 7;
-// let direccion;
+let texto = "Hola";
+let num = 7;
+let direccion;
 
 // console.log(texto);
 // console.log(num);
@@ -26,8 +26,30 @@
 
 // Ejercicio 4 clase 01
 
-// const precio = 10.99;
-// const cantidad = 3;
+const precio = 10.99;
+const cantidad = 3;
 
-// const total = precio * cantidad;
-// console.log("El total es:" , total);
+const total = precio * cantidad;
+console.log("El total es:" , total);
+
+// Ejercicio 5 clase 01
+
+let nota1 = 85;
+let nota2 = 90;
+let nota3 = 78;
+
+let promedio = (nota1 + nota2 + nota3)/3;
+console.log(promedio);
+
+// Ejercicio 6
+
+let nombreAlumno ="Álvaro";
+let edadAlumno = 36;
+let cursoAlumno = "Desarrollo web";
+
+console.log("Nombre:", nombreAlumno);
+console.log("Edad:", edadAlumno);
+console.log("Curso:", cursoAlumno);
+
+// Ejercicio 7
+

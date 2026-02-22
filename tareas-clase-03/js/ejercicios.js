7a1e3d74d0de6ca107958f890aaba13f98eeb5f0
Álvaro Benitez Mayorga <alvarobenitezmayorga@gmail.com>
2026-02-22 12:18:24 +0100

    clase 04

--- tareas-clase-03/js/ejercicios.js
+++ tareas-clase-03/js/ejercicios.js
@@ -19,21 +19,21 @@
 //  ------- Ejercicio 3 Menu simple
 // Usar do while.Mostrar un prompt con opciones 1. Saludar y 2. Salir. Repetir hasta que elijan salir
 
-let op;
+// let op;
 
-do {
-  op = prompt("Elige: 1.Saludar o 2.Salir");
-  switch (op) {
-    case 1:
-      op = prompt("Hola de nuevo, ¿quieres salir?");
-      break;
-    case 2:
-      op = prompt("Hasta pronto");
-      break;
-    default:
-      op = prompt("Opción no valida, ingrese una de las dos");
-  }
-} while (op != 2);
+// do {
+//   op = prompt("Elige: 1.Saludar o 2.Salir");
+//   switch (op) {
+//     case 1:
+//       op = prompt("Hola de nuevo, ¿quieres salir?");
+//       break;
+//     case 2:
+//       op = prompt("Hasta pronto");
+//       break;
+//     default:
+//       op = prompt("Opción no valida, ingrese una de las dos");
+//   }
+// } while (op != 2);
 
 // ------- Ejercicio 4 Contador inverso
 // Usar for. Mostrar números del 20 al 0. Agregar un comentario explicando que hace cada parte del for
---
+++ tareas-clase-04/js/scripts.js
@@ -0,0 +1,31 @@
+// ---- FUNCIONES ----
+
+// -- Funciones declaradas --
+
+// function saludar() {
+//   console.log("Hola, bienvenidos a la clase 04 de JS");
+// }
+
+// saludar(); // Llamada a la funcion
+// saludar();
+
+// function hola() {
+//   console.log("Hola mundo");
+// }
+
+// hola();
+// hola();
+
+// function saludar(nombre) {
+//   console.log("Hola", nombre);
+// }
+
+// saludar("María");
+
+function sumar(num1, num2) {
+  const suma = num1 + num2;
+  console.log("La suma es:", suma);
+}
+
+sumar(5, 10);
+sumar(36, 30);

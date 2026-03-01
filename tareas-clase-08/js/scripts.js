5dabb95a93e7a9400e5194e3d067f0f4e5382541
Álvaro Benitez Mayorga <alvarobenitezmayorga@gmail.com>
2026-03-01 19:28:08 +0100

    clase 08

---
+++ tareas-clase-08/js/scripts.js
@@ -0,0 +1,23 @@
+// --- classList ---
+
+// const p = document.querySelector("p");
+// console.log(p.classList);
+
+// -- classList.add() --
+// p.classList.add("activo");
+
+// -- classList.remove() --
+// p.classList.remove("activo");
+
+// -- classLista.contains() --
+// console.log(p.classList.contains("activo"));
+
+// -- classList.toggle() --
+// const p = document.querySelector("p");
+// p.classList.toggle("activo");
+// p.classList.toggle("activo");
+
+// -- classList.replace() --
+
+const p = document.querySelector("p");
+p.classList.replace("item", "destacado");

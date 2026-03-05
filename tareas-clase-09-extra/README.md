14b85b81a48d407b2261a29d60edb41fe15dd54e
Álvaro Benitez Mayorga <alvarobenitezmayorga@gmail.com>
2026-03-05 21:54:48 +0100

    clase 09 extra

---
+++ tareas-clase-09-extra/README.md
@@ -0,0 +1,10 @@
+# Clase 09 Extra
+
+- **Variable** ----> guardar un dato
+- **Condicionales(`if`)** ----> decidir algo
+- **Bucle (`for`)** ----> repetir algo
+- **Función** ----> ordenar o reutilizar lógica
+- **DOM** ---> mostrar / cambiar algo en la página
+- **Eventos** ----> esperar que el usuario haga algo
+- **Objetos literales** ----> agrupar datos relacionadas
+- **Array** ----> agrupar datos similares
---
+++ tareas-clase-09-extra/js/scripts.js
@@ -0,0 +1,18 @@
+const personas = [
+  //crea un array con tres objetos(personas)
+  { nombre: "Álvaro", edad: 36, admin: true }, // objeto 1
+  { nombre: "María", edad: 30, direccion: "Calle Marmoles" }, // objeto 2
+  { nombre: "Candela", edad: 10 }, // objeto 3
+];
+
+// console.log(personas);
+
+function esMayor(persona) {
+  if (persona.edad >= 18) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(esMayor(personas[1]));

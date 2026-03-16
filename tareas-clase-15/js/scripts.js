f484b6d948a5747b02293f8ed5a96abdb371706d
Álvaro Benitez Mayorga <alvarobenitezmayorga@gmail.com>
2026-03-16 21:23:52 +0100

    actualizacion

--- tareas-clase-15/js/scripts.js
+++ tareas-clase-15/js/scripts.js
@@ -1,14 +1,35 @@
-async function mostrarProducto() {
-  const response = await fetch("https://fakestoreapi.com/products/1");
-  const product = await response.json();
-  console.log(product);
+async function mostrarProductos() {
+  const response = await fetch("https://fakestoreapi.com/products/");
+  const products = await response.json();
+  console.log(products);
 
-  document.querySelector("#titulo").textContent = product.title;
-  document.querySelector("#precio").textContent = `$ ${product.price}`;
+  const container = document.querySelector("#productos");
+  container.innerHTML = "";
 
-  const imagen = document.querySelector("#imagen");
-  imagen.src = product.image;
-  imagen.alt = product.title;
+  products.forEach((product) => {
+    const card = document.createElement("article");
+    card.classList.add("card");
+
+    const imagen = document.createElement("img");
+    const h3 = document.createElement("h3");
+    const price = document.createElement("p");
+
+    imagen.src = product.image;
+    imagen.alt = product.title;
+    imagen.width = 150;
+
+    h3.textContent = product.title;
+
+    price.textContent = product.price;
+
+    card.appendChild(imagen);
+    card.appendChild(h3);
+    card.appendChild(price);
+
+    console.log(card);
+
+    container.appendChild(card);
+  });
 }
 
-mostrarProducto();
+mostrarProductos();
